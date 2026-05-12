131ec14592affb62dcccda131955b35adc7dfcc3
codefor-me <kanyinsolajoseph8@gmail.com>
2026-05-12 02:41:44 +0100

    Created output_module.py

---
+++ output_module.py
@@ -0,0 +1,7 @@
+def display_result(Student_name,Student_score,grade):
+
+    print("---------------Student data---------------")
+    print(f"Name: {Student_name}")
+    print(f"Score:{Student_score}")
+    print(f"Grade: {grade}")
+   
